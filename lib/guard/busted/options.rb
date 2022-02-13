f60c10fbfd6e23dbefca6a63cb7141592dcb2ed0
Piotr Jezusek <piotr.jezusek@softbeam.pl>
2022-02-13 14:06:34 +0100

    [#12] Fix a problem with a command structure that is passed to Open3

--- lib/guard/busted/options.rb
+++ lib/guard/busted/options.rb
@@ -4,10 +4,10 @@ module Guard
   module BustedOptions
     DEFAULTS = {
       run_all_on_start: false,
-      cmd: 'busted -o utfTerminal',
-      cmd_options: [],
-      cmd_all: 'busted -o utfTerminal',
-      cmd_all_options: []
+      cmd: 'busted',
+      cmd_options: ['-o', 'utfTerminal'],
+      cmd_all: 'busted',
+      cmd_all_options: ['-o', 'utfTerminal']
     }.freeze
   end
 end
